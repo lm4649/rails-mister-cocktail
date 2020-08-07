49eb1e4a71c46ac9cb4296f6f7bcf0bd2a548b25
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-07 15:39:58 +0900

    root to cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to 'cocktails#index'
   resources :doses, only: [:destroy]
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
